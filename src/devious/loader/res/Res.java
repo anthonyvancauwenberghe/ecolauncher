8265dff98f376448ec3b73c3ab4a6d924de1bea8
tony <anthony.vanc@gmail.com>
2016-05-26 21:49:00 +0200

    initial push

---
+++ src/devious/loader/res/Res.java
@@ -0,0 +1,14 @@
+package devious.loader.res;
+
+import javax.swing.ImageIcon;
+
+public final class Res {
+
+    public static final ImageIcon BANNER = new ImageIcon(Res.class.getResource("banner.png"));
+    public static final ImageIcon REFRESH_16 = new ImageIcon(Res.class.getResource("refresh16.png"));
+    public static final ImageIcon PLAY_16 = new ImageIcon(Res.class.getResource("play16.png"));
+    public static final ImageIcon ICON = new ImageIcon(Res.class.getResource("icon.png"));
+
+    private Res() {
+    }
+}
